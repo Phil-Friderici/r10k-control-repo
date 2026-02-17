fd71abb70091df54df3f3bab170d7af2ab9b2340
Phil Friderici <phil.friderici@perforce.com>
2026-02-17 07:57:03 +0000

    Let compilers use new PuppetDB

--- site/profile/manifests/compiler.pp
+++ site/profile/manifests/compiler.pp
@@ -1,3 +1,5 @@
 class profile::compiler {
-  include ::puppetdb::master::config
+  class { 'puppetdb::master::config':
+    puppetdb_server => 'ec2-3-254-122-1.eu-west-1.compute.amazonaws.com',
+  }
 }
