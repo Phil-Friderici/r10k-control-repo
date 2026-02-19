94d92ba9bf0b55f11ec68dbf308c77dc26c68026
Phil Friderici <phil.friderici@perforce.com>
2026-02-19 11:48:18 +0000

    Use theforman/puppet-puppet for compilers

--- site/profile/manifests/compiler.pp
+++ site/profile/manifests/compiler.pp
@@ -1,5 +1,17 @@
 class profile::compiler {
-  class { 'puppetdb::master::config':
-    puppetdb_server => 'ec2-3-254-122-1.eu-west-1.compute.amazonaws.com',
+
+  class { 'puppet':
+    server                => true,
+    server_foreman        => false,
+    server_reports        => 'puppetdb,store',
+    server_storeconfigs   => true,
+    server_external_nodes => '',
+    # use Puppetmaster as CA
+    server_ca             => false,
+    ca_server             => 'ec2-3-254-160-41.eu-west-1.compute.amazonaws.com', # CA runs on Puppetmaster
+  }
+
+  class { 'puppet::server::puppetdb':
+    server => 'ec2-3-254-160-41.eu-west-1.compute.amazonaws.com', # PuppetDB runs on Puppetmaster
   }
 }
