cb82a51d04576d80b849b2000c9b330a44e9e537
Phil Friderici <phil.friderici@perforce.com>
2026-02-19 12:29:50 +0000

    Use new compilers for loadbalancer

--- site/profile/manifests/haproxy.pp
+++ site/profile/manifests/haproxy.pp
@@ -10,15 +10,15 @@ class profile::haproxy {
   }
   haproxy::balancermember { 'compiler01':
     listening_service => 'puppet-agent',
-    server_names      => 'ec2-108-130-181-44.eu-west-1.compute.amazonaws.com',
-    ipaddresses       => '108.130.181.44',
+    server_names      => 'ec2-34-253-140-100.eu-west-1.compute.amazonaws.com',
+    ipaddresses       => '34.253.140.100',
     ports             => [8140],
     options           => 'check',
   }
   haproxy::balancermember { 'compiler02':
     listening_service => 'puppet-agent',
-    server_names      => 'ec2-18-203-244-108.eu-west-1.compute.amazonaws.com',
-    ipaddresses       => '18.203.244.108',
+    server_names      => 'ec2-34-241-190-180.eu-west-1.compute.amazonaws.com',
+    ipaddresses       => '34.241.190.180',
     ports             => [8140],
     options           => 'check',
   }
@@ -37,15 +37,15 @@ class profile::haproxy {
   }
   haproxy::balancermember { 'pxp01':
     listening_service => 'puppet-pxp',
-    server_names      => 'ec2-108-130-181-44.eu-west-1.compute.amazonaws.com',
-    ipaddresses       => '108.130.181.44',
+    server_names      => 'ec2-34-253-140-100.eu-west-1.compute.amazonaws.com',
+    ipaddresses       => '34.253.140.100',
     ports             => [8142],
     options           => 'check',
   }
   haproxy::balancermember { 'pxp02':
     listening_service => 'puppet-pxp',
-    server_names      => 'ec2-18-203-244-108.eu-west-1.compute.amazonaws.com',
-    ipaddresses       => '18.203.244.108',
+    server_names      => 'ec2-34-241-190-180.eu-west-1.compute.amazonaws.com',
+    ipaddresses       => '34.241.190.180',
     ports             => [8142],
     options           => 'check',
   }
