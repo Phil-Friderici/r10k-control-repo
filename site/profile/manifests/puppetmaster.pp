08db7eadb4f3902a2da4f0a7c436721854d6068b
Phil Friderici <phil.friderici@perforce.com>
2026-02-19 11:15:54 +0000

    Add config to setup PuppetDB on Puppetmaster

--- site/profile/manifests/puppetmaster.pp
+++ site/profile/manifests/puppetmaster.pp
@@ -2,7 +2,13 @@ class profile::puppetmaster {
   class { 'puppet':
     server                => true,
     server_foreman        => false,
-    server_reports        => 'store',
+    server_reports        => 'puppetdb,store',
+    server_storeconfigs   => true,
     server_external_nodes => '',
+  }
+
+  include puppetdb
+  class { 'puppet::server::puppetdb':
+    server => 'ec2-3-254-160-41.eu-west-1.compute.amazonaws.com', # PuppetDB runs on Puppetmaster itself
   }
 }
