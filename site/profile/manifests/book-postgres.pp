ccb0666d8206eec7ceb14760f5734cd81257bc94
Phil Friderici <phil.friderici@perforce.com>
2026-02-20 12:51:20 +0000

    Add postgresql server for testing

---
+++ site/profile/manifests/book-postgres.pp
@@ -0,0 +1,16 @@
+class profile::book-postgres {
+  class { 'postgresql::globals':
+    encoding            => 'UTF-8',
+    locale              => 'en_US.UTF-8',
+    version             => '15',
+    manage_package_repo => true,
+  }
+  class { 'postgresql::server':
+    listen_addresses => '127.0.0.1',
+  }
+  postgresql::server::extension { 'pg_trgm':
+    database => 'puppetdb',
+    require  => Postgresql::Server::Db['puppetdb'],
+    before   => Service['puppetdb'],
+  }
+}
