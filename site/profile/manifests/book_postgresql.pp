8c51fa7c9ce8b037d47b4e3379bb740fc2701b04
Phil Friderici <phil.friderici@perforce.com>
2026-02-23 13:34:36 +0000

    postgresql: set max_connections to 200

--- site/profile/manifests/book_postgresql.pp
+++ site/profile/manifests/book_postgresql.pp
@@ -15,6 +15,11 @@ class profile::book_postgresql {
     listen_addresses => '172.31.31.135',
   }
 
+  # set from 100 to 200 to avoid "remaining connection slots are reserved for non-replication superuser connections"
+  postgresql::server::config_entry { 'max_connections':
+    value => 200,
+  }
+
   postgresql::server::extension { 'pg_trgm':
     database => 'puppetdb',
     require  => Postgresql::Server::Db['puppetdb'],
