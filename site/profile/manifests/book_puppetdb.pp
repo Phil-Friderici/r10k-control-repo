0e931e1222229cd781cb98a2bde5ca00f4e16f00
Phil Friderici <phil.friderici@perforce.com>
2026-02-24 11:50:22 +0000

    Allow PuppetDBs dashboard to listen to 0.0.0.0

--- site/profile/manifests/book_puppetdb.pp
+++ site/profile/manifests/book_puppetdb.pp
@@ -1,5 +1,6 @@
 class profile::book_puppetdb {
   class { 'puppetdb::server':
-    database_host => 'ec2-3-249-73-230.eu-west-1.compute.amazonaws.com',
+    database_host  => 'ec2-3-249-73-230.eu-west-1.compute.amazonaws.com',
+    listen_address => '0.0.0.0',
   }
 }
