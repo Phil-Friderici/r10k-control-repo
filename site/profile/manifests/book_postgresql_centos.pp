e9c677ce88674b6f9dfca47f8eb3857ff3c79af0
Phil Friderici <phil.friderici@perforce.com>
2026-02-26 12:08:43 +0000

    PostgreSQL: Fix parameters for 16.8 from appstream

--- site/profile/manifests/book_postgresql_centos.pp
+++ site/profile/manifests/book_postgresql_centos.pp
@@ -4,15 +4,20 @@ class profile::book_postgresql_centos {
     locale              => 'en_US.UTF-8',
     version             => '16',
     manage_package_repo => true,
+    datadir             => '/var/lib/pgsql/data',
+    initdb_path         => '/bin/initdb',
+    service_name        => 'postgresql',
+    server_package_name => 'postgresql-server', # package from appstream https://bckinfo.com/how-to-install-postgresql-16-3-on-centos-stream-9/
+    psql_path           => '/bin/psql',
   }
   class { 'postgresql::server':
-    listen_addresses        => '172.31.31.135',
+    listen_addresses        => '172.31.25.101',
     ip_mask_allow_all_users => '0.0.0.0/0',
   }
 
   class { 'puppetdb::database::postgresql':
     manage_server    => false,
-    listen_addresses => '172.31.31.135',
+    listen_addresses => '172.31.25.101',
   }
 
   # set from 100 to 200 to avoid "remaining connection slots are reserved for non-replication superuser connections"
