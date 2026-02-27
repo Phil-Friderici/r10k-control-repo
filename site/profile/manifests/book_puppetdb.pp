749904b587cc6294d47d5000a6e00d469a59d0ad
Phil Friderici <phil.friderici@perforce.com>
2026-02-27 11:02:07 +0000

    PuppetDB: use another PostgreSQL

--- site/profile/manifests/book_puppetdb.pp
+++ site/profile/manifests/book_puppetdb.pp
@@ -1,6 +1,6 @@
 class profile::book_puppetdb {
   class { 'puppetdb::server':
-    database_host  => 'ec2-34-244-198-2.eu-west-1.compute.amazonaws.com',
+    database_host  => 'ec2-54-170-238-132.eu-west-1.compute.amazonaws.com',
     listen_address => '0.0.0.0',
   }
 }
