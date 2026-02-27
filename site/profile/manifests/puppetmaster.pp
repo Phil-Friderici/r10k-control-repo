0767138ca496ed311f63f8a262a98c7695448d56
Phil Friderici <phil.friderici@perforce.com>
2026-02-27 13:04:09 +0000

    PuppetDB: use PuppetDB on Puppet Master itself

--- site/profile/manifests/puppetmaster.pp
+++ site/profile/manifests/puppetmaster.pp
@@ -9,7 +9,7 @@ class profile::puppetmaster {
 
   include puppetdb
   class { 'puppet::server::puppetdb':
-#    server => 'ec2-3-254-160-41.eu-west-1.compute.amazonaws.com', # PuppetDB runs on Puppetmaster itself
-    server => 'ec2-3-250-185-137.eu-west-1.compute.amazonaws.com', # PuppetDB runs on pf-book-puppetdb1
+    server => 'ec2-3-254-160-41.eu-west-1.compute.amazonaws.com', # PuppetDB runs on Puppetmaster itself
+#    server => 'ec2-3-250-185-137.eu-west-1.compute.amazonaws.com', # PuppetDB runs on pf-book-puppetdb1
   }
 }
