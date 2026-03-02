34792a18c332bd992f86df121d4fde8e59a36a49
Phil Friderici <phil.friderici@perforce.com>
2026-03-02 10:15:55 +0000

    Create profile for PuppetDB on CentOS and asigne node

---
+++ site/profile/manifests/book_puppetdb_centos.pp
@@ -0,0 +1,6 @@
+class profile::book_puppetdb_centos {
+  class { 'puppetdb::server':
+    database_host  => 'ec2-54-170-238-132.eu-west-1.compute.amazonaws.com',
+    listen_address => '0.0.0.0',
+  }
+}
