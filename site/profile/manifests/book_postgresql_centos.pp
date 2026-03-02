56213edfb597b8ab0d6d19b161021864e2d5b237
Phil Friderici <phil.friderici@perforce.com>
2026-03-02 15:01:27 +0000

    PostgreSQL: use 200 max_connections

--- site/profile/manifests/book_postgresql_centos.pp
+++ site/profile/manifests/book_postgresql_centos.pp
@@ -3,4 +3,9 @@ class profile::book_postgresql_centos {
     listen_addresses => '0.0.0.0',
     postgres_version => '16', # minor version is defined in node declaration in hiera (postgresql::globals::server_package_name & contrib_package_name)
   }
+
+  # set max connections from 100 to 200 to avoid "remaining connection slots are reserved for non-replication superuser connections"
+  postgresql::server::config_entry { 'max_connections':
+    value => 200,
+  }
 }
