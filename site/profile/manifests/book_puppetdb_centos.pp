ac2cb5cc4443a14e34cbceda1dadd22867449347
Phil Friderici <phil.friderici@perforce.com>
2026-03-04 13:17:42 +0000

    Refactor PuppetDB Dashboard access

--- site/profile/manifests/book_puppetdb_centos.pp
+++ site/profile/manifests/book_puppetdb_centos.pp
@@ -2,12 +2,6 @@ class profile::book_puppetdb_centos {
   class { 'puppetdb::server':
     database_host  => 'ec2-79-125-45-224.eu-west-1.compute.amazonaws.com',
     listen_address  => '0.0.0.0',
-  }
-
-  firewalld_port { 'Open port 8080 in the public zone':
-    ensure   => present,
-    zone     => 'public',
-    port     => 8080,
-    protocol => 'tcp',
+    open_listen_port => true,
   }
 }
