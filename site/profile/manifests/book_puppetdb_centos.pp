d8f58089acbd5206070707e0dce3022b9af005eb
Phil Friderici <phil.friderici@perforce.com>
2026-03-04 14:00:03 +0000

    Allow access to PuppetDB Dashboard on Port 8081

--- site/profile/manifests/book_puppetdb_centos.pp
+++ site/profile/manifests/book_puppetdb_centos.pp
@@ -1,9 +1,10 @@
 class profile::book_puppetdb_centos {
   class { 'puppetdb::server':
-    database_host     => 'ec2-79-125-45-224.eu-west-1.compute.amazonaws.com',
-    listen_address    => '0.0.0.0',
-    open_listen_port  => true,
-    command_threads   => 2,
-    concurrent_writes => 4,
+    database_host        => 'ec2-79-125-45-224.eu-west-1.compute.amazonaws.com',
+    listen_address       => '0.0.0.0',
+    open_ssl_listen_port => true,
+    open_listen_port     => true,
+    command_threads      => 2,
+    concurrent_writes    => 4,
   }
 }
