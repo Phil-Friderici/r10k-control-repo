faa814e1e560487af748adf69987f85ea952ec2d
Phil Friderici <phil.friderici@perforce.com>
2026-03-09 12:35:19 +0000

    Dashboard: prepare PostgreSQL server

--- site/profile/manifests/book_postgresql_centos.pp
+++ site/profile/manifests/book_postgresql_centos.pp
@@ -1,11 +1,15 @@
 class profile::book_postgresql_centos {
   class { 'puppetdb::database::postgresql':
-    listen_addresses => '0.0.0.0',
-    postgres_version => '16', # minor version is defined in node declaration in hiera (postgresql::globals::server_package_name & contrib_package_name)
+    listen_addresses  => '0.0.0.0',
+    postgres_version  => '16', # minor version is defined in node declaration in hiera (postgresql::globals::server_package_name & contrib_package_name)
+    postgresql_ssl_on => true,
   }
 
   # set max connections from 100 to 200 to avoid "remaining connection slots are reserved for non-replication superuser connections"
   postgresql::server::config_entry { 'max_connections':
     value => 1000,
   }
+
+  # Dashboard - collect PostgreSQL metrics
+  include puppet_operational_dashboards::profile::foss_postgres_access
 }
