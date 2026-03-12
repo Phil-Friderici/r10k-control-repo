d975a67d991bda9ec599df87618dd5b139032dd9
Phil Friderici <phil.friderici@perforce.com>
2026-03-12 09:11:47 +0000

    PostgreSQL: use new node

--- site/profile/manifests/book_puppetdb_centos.pp
+++ site/profile/manifests/book_puppetdb_centos.pp
@@ -1,6 +1,6 @@
 class profile::book_puppetdb_centos {
   class { 'puppetdb::server':
-    database_host        => 'ec2-18-201-130-210.eu-west-1.compute.amazonaws.com',
+    database_host        => 'ec2-52-31-116-165.eu-west-1.compute.amazonaws.com',
     listen_address       => '0.0.0.0',
     open_ssl_listen_port => true,
     open_listen_port     => true,
