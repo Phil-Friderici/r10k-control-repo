a857c8c8aec53e1a1078f5389292287c0e4ca14d
Phil Friderici <phil.friderici@perforce.com>
2026-03-16 15:30:27 +0000

    AIO: change PuppetDB node for puppetserver usage

--- site/profile/manifests/book_aio_puppetserver.pp
+++ site/profile/manifests/book_aio_puppetserver.pp
@@ -9,6 +9,6 @@ class profile::book_aio_puppetserver {
 
   include puppetdb
   class { 'puppet::server::puppetdb':
-    server => 'ec2-54-170-24-140.eu-west-1.compute.amazonaws.com', # PuppetDB runs on Puppetmaster itself
+    server => 'ec2-108-130-189-34.eu-west-1.compute.amazonaws.com', # PuppetDB runs on Puppetmaster itself
   }
 }
