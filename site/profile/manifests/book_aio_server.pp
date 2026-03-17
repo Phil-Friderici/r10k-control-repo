5eabdbb9c7a705c2006a2fcd7a4e65d433ce2cbf
Phil Friderici <phil.friderici@perforce.com>
2026-03-17 15:27:25 +0000

    AIO: fix settings for server

--- site/profile/manifests/book_aio_server.pp
+++ site/profile/manifests/book_aio_server.pp
@@ -1,17 +1,28 @@
 class profile::book_aio_server {
   class { 'puppet':
-    server                => true,
-    server_foreman        => false,
-    server_reports        => 'puppetdb,store',
-    server_storeconfigs   => true,
-    server_external_nodes => '',
+    server                     => true,
+    ca_server                  => 'ec2-108-130-189-34.eu-west-1.compute.amazonaws.com',
+    server_ca                  => false,
+    server_foreman             => false,
+    server_reports             => 'puppetdb,store',
+    server_storeconfigs        => true,
+    server_external_nodes      => '',
+    agent_additional_settings  => {server => puppet},
+    server_additional_settings => {storeconfigs_backend => 'puppetdb'},
   }
 
-  class { 'puppetdb':
-    manage_firewall => false,
+  class { 'puppetdb::master::config':
+    puppetdb_server     => $facts['networking']['fqdn'],
+    manage_storeconfigs => false,
   }
 
-  class { 'puppet::server::puppetdb':
-    server => '127.0.0.1', # PuppetDB runs locally
+  class { 'puppetdb::server':
+    database_host        => 'ec2-52-211-193-160.eu-west-1.compute.amazonaws.com',
+    manage_firewall      => false,
+    listen_address       => '0.0.0.0',
+    open_ssl_listen_port => true,
+    open_listen_port     => true,
+    command_threads      => 2,
+    concurrent_writes    => 4,
   }
 }
